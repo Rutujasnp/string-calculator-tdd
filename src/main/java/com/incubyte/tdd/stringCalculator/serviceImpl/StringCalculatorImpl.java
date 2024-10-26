7267bd86cb5b4d2a8411a428e3073a30cde60300
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-10-26 00:02:07 -0700

    implemented logic to pass test for custom delimiter

--- src/main/java/com/incubyte/tdd/stringCalculator/serviceImpl/StringCalculatorImpl.java
+++ src/main/java/com/incubyte/tdd/stringCalculator/serviceImpl/StringCalculatorImpl.java
@@ -2,30 +2,35 @@ package com.incubyte.tdd.stringCalculator.serviceImpl;
 
 import com.incubyte.tdd.stringCalculator.service.StringCalculator;
 
-public class StringCalculatorImpl  implements  StringCalculator {
+public class StringCalculatorImpl implements StringCalculator {
 
 	@Override
 	public int add(String numbers) {
-		
-		//if the string of numbers is empty
-		if(numbers.isEmpty() || numbers.equals(" ")) {
+
+		// if the string of numbers is empty
+		if (numbers.isEmpty() || numbers.equals(" ")) {
 			return 0;
 		}
-		
-		
-		
-		//for numbers string has newline delimiter
-		
-		if (numbers.contains("\n")) {
-			numbers=numbers.replace("\n", ",");
+
+		// for numbers string has newline delimiter
+
+//		if (numbers.contains("\n")) {
+//			numbers=numbers.replace("\n", ",");
+//		}
+		// for custom delimiters
+		String delimiter = ",|\n";
+		int delimiterIndex = numbers.indexOf("\n");
+		if (numbers.startsWith("//")) {
+			delimiter = numbers.substring(2, delimiterIndex);
+			numbers= numbers.substring(delimiterIndex+1);
 		}
-		String delimiter= ",";
-		int sum=0;
-		String numbersArray[]=numbers.split(delimiter);
-		for (String numString: numbersArray){
-			int number= Integer.parseInt(numString);
-			sum+=number;
-			
+
+		int sum = 0;
+		String numbersArray[] = numbers.split(delimiter);
+		for (String numString : numbersArray) {
+			int number = Integer.parseInt(numString.trim());
+			sum += number;
+
 		}
 		return sum;
 	}
