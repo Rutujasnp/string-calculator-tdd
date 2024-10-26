e0e1257885fb4c781b200e50c6017b98a0c86836
Rutujasnp <65347296+Rutujasnp@users.noreply.github.com>
2024-10-26 00:09:12 -0700

    added test to check negative numbers

--- src/test/java/com/incubyte/tdd/stringCalculator/StringCalculatorApplicationTests.java
+++ src/test/java/com/incubyte/tdd/stringCalculator/StringCalculatorApplicationTests.java
@@ -9,38 +9,43 @@ import com.incubyte.tdd.stringCalculator.serviceImpl.StringCalculatorImpl;
 @SpringBootTest
 class StringCalculatorApplicationTests {
 
-	private final StringCalculatorImpl stringCalculatorImpl=new StringCalculatorImpl();
+	private final StringCalculatorImpl stringCalculatorImpl = new StringCalculatorImpl();
+
 	@Test
 	public void testEmptyStringNumbers() {
 		Assertions.assertEquals(0, stringCalculatorImpl.add(""));
 	}
 
-	
-	
-	@Test 
+	@Test
 	public void testSingleNumberInput() {
 		Assertions.assertEquals(1, stringCalculatorImpl.add("1"));
 	}
-	
+
 	@Test
 	public void testTwoNumberInput() {
 		Assertions.assertEquals(3, stringCalculatorImpl.add("1,2"));
 	}
-	
+
 	@Test
 	public void testMultipleNumberInput() {
 		Assertions.assertEquals(10, stringCalculatorImpl.add("1,2,3,4"));
 	}
-	
+
 	@Test
 	public void testNewlineDelimiterInNumberString() {
 		Assertions.assertEquals(6, stringCalculatorImpl.add("1\n2,3"));
 	}
-	
+
 	@Test
 	public void testCustomDelimiterInNumberString() {
 		Assertions.assertEquals(3, stringCalculatorImpl.add("//;\n1;2"));
 	}
+
+	@Test
+	public void testNegativeNumberInNumberString() {
+		Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			stringCalculatorImpl.add("-1,4,-3");
+		});
+		Assertions.assertEquals("negative numbers not allowed: [-1, -3]", exception.getMessage());
+	}
 }
-
-
